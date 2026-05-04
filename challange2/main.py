41fafd1544cd0236feff808e5dd2241490a38c25
FernandoIGD <fernando.igd@outlook.com>
2026-05-03 21:33:30 -0500

    fix: specify HTTP methods for hires-by-quarter and depts-above-mean routes

--- challange2/main.py
+++ challange2/main.py
@@ -3,11 +3,11 @@ from service import get_employees_for_each_job, list_ids_names_numbers
 
 app = Flask(__name__)
 
-@app.route("/hires-by-quarter")
+@app.route("/hires-by-quarter", methods=["GET"])
 def hires_by_quarter():
     return jsonify(get_employees_for_each_job())
 
-@app.route("/depts-above-mean")
+@app.route("/depts-above-mean", methods=["GET"])
 def depts_above_mean():
     return jsonify(list_ids_names_numbers())
     
